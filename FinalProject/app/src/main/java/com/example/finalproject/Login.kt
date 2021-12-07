3e60471a087d413b849a227f494883e998596f3a
Catherine0525 <20192643@s.ubaguio.edu>
2021-12-08 00:23:25 +0800

    Login and Signup works? Logout not yet implemented

--- FinalProject/app/src/main/java/com/example/finalproject/Login.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/Login.kt
@@ -1,20 +1,117 @@
 package com.example.finalproject
 
+import android.app.ProgressDialog.show
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
+import com.example.finalproject.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var email:String
+    private lateinit var password:String
+
+    private lateinit var actionBar: ActionBar
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val viewSignup = findViewById<TextView>(R.id.tvLogin)
+        auth = FirebaseAuth.getInstance()
+        checkUser()
 
-        viewSignup?.setOnClickListener{
-            val intent = Intent(this, Signup::class.java)
-            startActivity(intent)
+        actionBar=supportActionBar!!
+        actionBar.title="Login"
+
+        binding.loginBtn.setOnClickListener{
+            validateData()
+        }
+
+        binding.tvSignup.setOnClickListener{
+            navToSignup()
+        }
+
+    }
+
+    private fun navToSignup() {
+        startActivity(Intent(this, Signup::class.java))
+    }
+
+    private fun validateData() {
+        email=binding.etEmail.text.toString().trim()
+        password= binding.etPassword.text.toString().trim()
+
+        //validate input
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmail.error="Invalid email" //displays in the editTextView
+            show("Email invalid.", "Please check your input and try again.")
+        }
+        //validate if textfield is empty
+        else if((TextUtils.isEmpty(email)||(TextUtils.isEmpty(password)))){
+            //do the same as in email, use a different message for binding and show()
+            if(TextUtils.isEmpty(email)){
+                binding.etEmail.error="Input Email" //displays in the editTextView
+                show("Email empty.", "Please input your email.")
+            }else{
+                binding.etEmail.error="Input Password" //displays in the editTextView
+                show("Password empty.", "Please input your password.")
+            }
+        }else{
+            //for valid input, invoke Login function
+            show("Please wait.", "Logging in....")
+            firebaselogin()
+        }
+    }
+
+    private fun firebaselogin() {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                //when login credentials are successfully validated
+                //start by getting the users acct information from firebase
+                val firebaseUser=auth.currentUser
+                val email=firebaseUser!!.email
+                show("Welcome","$email")
+
+                //redirect user to profile page
+                startActivity(Intent(
+                    this,
+                    Profile::class.java))
+                finish() //closes this activity
+            }.addOnFailureListener { e->
+                //when login credentials are not found
+                // e.message shows cause of error
+                show("Login Failed.","${e.message}")
+            }
+    }
+
+    private fun show(title:String, message:String){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("OK", null) //user needs to click on "Ok" Button to dismiss
+        builder.setCancelable(false)    //does not allow dialog to be cancelled
+
+        val dialog=builder.create()
+        dialog.show()
+    }
+
+    private fun checkUser(){
+        val firebaseUser=auth.currentUser
+        if(firebaseUser != null){
+            startActivity(Intent(
+                this,
+                Profile::class.java
+            ))
+            finish()
         }
     }
 }
--- FinalProject/app/src/main/java/com/example/finalproject/Signup.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/Signup.kt
@@ -3,18 +3,117 @@ package com.example.finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
+import com.example.finalproject.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Signup : AppCompatActivity() {
+    private lateinit var binding:ActivitySignupBinding
+    private lateinit var email:String
+    private lateinit var password:String
+
+    private lateinit var actionBar: ActionBar
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding= ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val viewLogin = findViewById<TextView>(R.id.tvLogin)
+        email = binding.etEmail.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
 
-        viewLogin?.setOnClickListener{
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
+        actionBar=supportActionBar!!
+        actionBar.title=("Signup")
+
+        actionBar.setDisplayShowHomeEnabled(true)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+        auth = FirebaseAuth.getInstance()
+        binding.btnSignup.setOnClickListener{
+            //invoke the signup function
+            firebaseSignUp()
         }
+        binding.tvLogin.setOnClickListener{
+            //if the user already has an account, validate
+            validateAccount()
+        }
+    }
+
+    private fun show(title:String, message:String){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("OK", null) //user needs to click on "Ok" Button to dismiss
+        builder.setCancelable(false)    //does not allow dialog to be cancelled
+
+        val dialog=builder.create()
+        dialog.show()
+    }
+
+    private fun validateAccount() {
+        email=binding.etEmail.text.toString().trim()
+        password= binding.etPassword.text.toString().trim()
+
+        //validate input
+        //Email validation | checks email input pattern
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmail.error="Invalid email" //displays in the editTextView
+            show("Email invalid.", "Please check your input and try again.")
+        }
+        //validate if textfield is empty
+        else if((TextUtils.isEmpty(email)||(TextUtils.isEmpty(password)))){
+            //do the same as in email, use a different message for binding and show()
+            if(TextUtils.isEmpty(email)){
+                binding.etEmail.error="Input Email" //displays in the editTextView
+                show("Email empty.", "Please input your email.")
+            }else{
+                binding.etEmail.error="Input Password" //displays in the editTextView
+                show("Password empty.", "Please input your password.")
+            }
+        }else{
+            //for valid input, invoke Login function
+            show("Please wait.", "Logging in....")
+            firebaselogin()
+        }
+    }
+
+    private fun firebaselogin() {
+        TODO("Not yet implemented")
+    }
+
+    private fun firebaseSignUp() {
+        email=binding.etEmail.text.toString().trim()
+        password= binding.etPassword.text.toString().trim()
+
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                //add user account details to firebase ->from email and password input
+                val firebaseUser=auth.currentUser
+                val email=firebaseUser!!.email
+
+                //alert if account is created
+                show("Success", "Account created, welcome $email")
+
+                //redirect user to his/her default page or profile page
+                startActivity(Intent(this, Profile::class.java))
+                finish()
+            }
+            //failed Signup
+            .addOnFailureListener { e->
+                show("Signup Failed!", "Signup process encountered an error. "+
+                        "${e.message}")
+            }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        //for action bar
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
